61b567935a9575b3d20934e94a03a87ef433bc1a
RSDarcy <39503717+RSDarcy@users.noreply.github.com>
2020-02-20 17:55:47 -0400

    Commit  Practica!
    Prueba parcial 1

---
+++ DejalosCaer/Assets/Scripts/Control.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour {
+    public float velocidadX = 0.1f;
+    private Vector3 translate;
+    private const float limiI = -2.6f;
+    private const float LimiD = 2.6f;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        translate = new Vector3(Input.GetAxis("Horizontal") * velocidadX ,0);
+        gameObject.transform.Translate(translate);
+
+        gameObject.transform.position = new Vector3(Mathf.Clamp(gameObject.transform.position.x, limiI, LimiD), gameObject.transform.position.y, gameObject.transform.position.z);
+	}
+}
---
+++ DejalosCaer/Assets/Scripts/ScriptGlobal.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptGlobal : MonoBehaviour {
+    public GameObject CuboPrefab;
+    private Vector3 nuevaPos;
+    private const float limiI = -2.6f;
+    private const float limiD = 2.6f;
+    private float periodoInstancia = 1f;
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("InstanciarCubo", 0f, periodoInstancia);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void InstanciarCubo()
+    {
+        nuevaPos = new Vector3(Random.Range(limiI, limiD), 6f);
+        Instantiate(CuboPrefab, nuevaPos, Quaternion.identity);
+    }
+}
